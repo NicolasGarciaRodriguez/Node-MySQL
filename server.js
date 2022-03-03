78889cd2b6b4644f7c5791d6bc0db939d3db34fa
NicolasGarciaRodriguez <nicogarcia.ro97@gmail.com>
2022-03-03 10:45:57 +0100

    listo para deploy en heroku

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use((req, res, next) => {
 
 
 ////CONFIGURACION DEL PUERTO////
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
     console.log(`Escuchando en el puerto ${PORT}`)
 })
